0775af5f0f9ecdd71349b4f2bd8030cca2a4cd02
ShahriyarMammadov <shahriyarmammadov16@gmail.com>
2024-07-26 14:50:07 +0400

    drawer added

--- src/layouts/header/index.jsx
+++ src/layouts/header/index.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./index.scss";
 import { Link, NavLink } from "react-router-dom";
-import { FaMagnifyingGlass, FaBarsStaggered } from "react-icons/fa6";
+import {
+  FaMagnifyingGlass,
+  FaBarsStaggered,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa6";
+import { BsInstagram } from "react-icons/bs";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const drawerRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (drawerRef.current && !drawerRef.current.contains(event.target)) {
+      setOpen(false);
+    }
+  };
+
   return (
     <header>
       <div className="container">
@@ -23,10 +47,91 @@ const Header = () => {
             <FaMagnifyingGlass className="searchIcon" />
           </div>
 
-          <div className="menuBar">
+          <div className="menuBar" onClick={() => setOpen(true)}>
             <FaBarsStaggered />
           </div>
         </nav>
+      </div>
+
+      <div id="drawer" className={open ? "open" : "closed"} ref={drawerRef}>
+        <div className={open ? "drawerContent  open" : "drawerContent closed"}>
+          <div className="head">
+            <h4>MENU</h4>
+            <div className="closeIcon" onClick={() => setOpen(false)}>
+              x
+            </div>
+          </div>
+
+          <nav>
+            <div
+              onClick={() => {
+                setSubMenu(subMenu === 1 ? 0 : 1);
+              }}
+            >
+              <Link to={"/haqqimizda"}>Haqqımızda</Link>
+            </div>
+
+            <Link to={"/rezervation"} onClick={() => setOpen(false)}>
+              Rezervasiya
+            </Link>
+
+            <div
+              onClick={() => {
+                setSubMenu(subMenu === 2 ? 0 : 2);
+              }}
+            >
+              <Link to={"/services"}>Xidmətlər</Link>
+            </div>
+
+            <div
+              onClick={() => {
+                setSubMenu(subMenu === 2 ? 0 : 2);
+              }}
+            >
+              <Link to={"/contact"}>Əlaqə</Link>
+            </div>
+          </nav>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam
+            alias nam voluptate accusamus vitae at vero mollitia cumque ducimus
+            assumenda.
+          </p>
+
+          <div className="socialMediaIcons">
+            <a
+              href="https://www.facebook.com/NatureFriends"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebookF className="socialIcons" />
+            </a>
+
+            <a
+              href="https://www.instagram.com/naturefriendsaz/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsInstagram className="socialIcons" />
+            </a>
+
+            <a
+              href="https://www.facebook.com/NatureFriends"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedinIn className="socialIcons" />
+            </a>
+
+            <a
+              href="https://www.facebook.com/NatureFriends"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter className="socialIcons" />
+            </a>
+          </div>
+        </div>
       </div>
     </header>
   );
